380c33ab209f4f3a89f3ed29cd46e3ee3664caae
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-10-20 19:44:37 -0500

    Se cambió de forma y se agregó stroke

--- sketch.js
+++ sketch.js
@@ -1,35 +1,48 @@
-let W1;
+let w1;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  W1 = new Walker(200, 200);
+	createCanvas(windowWidth, windowHeight);
+	w1 = new Walker(200, 200);
 }
 
 function draw() {
-  W1.update();
-  W1.display();
+	w1.update();
+	w1.display();
+	
 }
+
 class Walker {
-  constructor(_x, _y) {
-    this.posx = _x;
-    this.posy = _y;
-    this.vel = random(1, 10);
-    this.color = color(255, random(0, 200), random(0, 200));
-    this.esCirculo = random() > 0.5 ? true : false;
-  }
+	constructor(_x, _y) {
+		this.posx = _x;
+		this.posy = _y;
+		this.vel = random(1, 9);
+		this.color = color(0, random(100, 255), random(255, 0));
 
-  update() {
-    this.posx += random(-this.vel, this.vel);
-    this.posy += random(-this.vel, this.vel);
-  }
+		this.esellipse = random() > 0.5 ? true : false;
 
-  display() {
-    fill(this.color);
-    noStroke();
-    if (this.esCirculo) {
-      circle(this.posx, this.posy, 10);
-    } else {
-      rect(this.posx, this.posy, 10, 10);
-    }
-  }
+		// this.esCirculo;
+		// this.r = random();
+		// if (this.r > 0.5) {
+		// 	this.esCirculo = true;
+		// } else {
+		// 	this.esCirculo = false;
+		// }
+	}
+
+	update() {
+		this.posx += random(-this.vel, this.vel);
+		this.posy += random(-this.vel, this.vel);
+	}
+
+	display() {
+		fill(this.color);
+		stroke('#fae');
+		strokeWeight(1.5);
+		if (this.esCirculo) {
+			circle(this.posx, this.posy, 50);
+		} else {
+			ellipse(this.posx, this.posy, 25, 40);
+		}
+	}
 }
+
